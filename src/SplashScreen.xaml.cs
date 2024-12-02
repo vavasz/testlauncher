e8d0229fdd74203cc8445f8d2198dc9e95e44b25
Clocksz <wmshuee@gmail.com>
2024-12-02 04:52:36 -0300

    source

--- src/SplashScreen.xaml.cs
+++ src/SplashScreen.xaml.cs
@@ -18,7 +18,7 @@ namespace CanaryLauncherUpdate
 {
 	public partial class SplashScreen : Window
 	{
-		static string launcerConfigUrl = "https://raw.githubusercontent.com/opentibiabr/canary-launcher/main/launcher_config.json";
+		static string launcerConfigUrl = "https://github.com/vavasz/testlauncher/blob/main/launcher_config.json";
 		// Load informations of launcher_config.json file
 		static ClientConfig clientConfig = ClientConfig.loadFromFile(launcerConfigUrl);
 
